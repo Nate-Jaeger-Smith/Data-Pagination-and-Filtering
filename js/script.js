f239fba75a267668da3bc19f93921709e12603a5
Nate Jaeger-Smith <157942163+Nate-Jaeger-Smith@users.noreply.github.com>
2024-01-31 19:11:46 -0600

    Added showPage function
    Successfully adds correct amount of students per page at specified starting and end points

--- js/script.js
+++ js/script.js
@@ -1,24 +1,35 @@
 /*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
-
-
-
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
-
-
-
-/*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+const itemsPerPage = 9;
+function showPage (list, page){
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * itemsPerPage;
 
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML = "";
 
+   for ( let i = 0; i < list.length; i++ ) {
+      let student = list[i];
+      if ( i >= startIndex && i < endIndex ) {
+         const studentCard = `<li class="student-item cf">
+         <div class="student-details">
+           <img class="avatar" src="${student.picture.thumbnail}" alt="Profile Picture">
+           <h3>${student.name.first} ${student.name.last}</h3>
+           <span class="email">${student.email}</span>
+         </div>
+         <div class="joined-details">
+           <span class="date">Joined ${student.registered.date}</span>
+         </div>
+       </li>`;
+
+       studentList.insertAdjacentHTML('beforeend', studentCard);
+      }
+   }
+}
+
+showPage(data, 2);
 
 /*
 Create the `addPagination` function
